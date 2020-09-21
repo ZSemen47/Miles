ad8d51cb28fadcef3851566ca90399c2471babcf
Semen Zazulin <zazulinsemen@gmail.com>
2020-09-21 19:56:40 +0300

    src and gitignore

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        int price_of_ticket = 1000;
+        int convertasion_to_mile = 20;
+        int bonus = price_of_ticket / convertasion_to_mile;
+        System.out.println(bonus);
+    }
+}
